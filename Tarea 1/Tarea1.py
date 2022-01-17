39e16fdef983aa2ad1daecddbf1dbed1255afe19
Nickolas Nolte <61555652+nickolasnolte1@users.noreply.github.com>
2022-01-16 18:43:11 -0600

    Add files via upload

---
+++ Tarea 1/Tarea1.py
@@ -0,0 +1,9 @@
+# Nickolas Nolte
+# Tarea 1
+
+x=int(input("Ingrese un número: "))
+suma = 0
+while(x > 0):
+    suma=suma+x
+    x=x-1
+print("La suma de los primeros n números naturales es: ",suma)
