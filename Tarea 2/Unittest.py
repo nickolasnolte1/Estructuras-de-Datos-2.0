eef8add4a34eb69eae0a9b5474795dd08802e81c
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-02-03 01:34:27 -0600

    update

--- Tarea 2/Unittest.py
+++ Tarea 2/Unittest.py
@@ -1,13 +1,14 @@
 import unittest
-import Practica2
+from programa import Practica2
 
 
 class TestStringMethods(unittest.TestCase):
 
 
-    def agregar_debitos(self):
+    def saldo(self):
         assert Practica2.sum(50) == 300
         self.assertEqual( Practica2.sum(20), 210)
         
 if __name__ == '__main__':
     unittest.main()
+    
