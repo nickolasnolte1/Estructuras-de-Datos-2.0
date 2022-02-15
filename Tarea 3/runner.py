e3e5f89cacbbb1e400d4a206c4c4f84099abac15
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-02-15 00:12:37 -0600

    .

--- Tarea 3/runner.py
+++ Tarea 3/runner.py
@@ -6,19 +6,19 @@ x=int(input("Elija de qué figura quiere calcular el área: "))
 
 if x==1:
 
-    radio=int(input('Ingrese el radio del circulo: '))
+    radio=float(input('Ingrese el radio del circulo: '))
     funciones.area_circulo(radio)
     
 if x==2:
     
-    base1=int(input('Ingrese la base del triángulo: '))
-    h1=int(input('Ingrese la altura del triángulo: '))
+    base1=float(input('Ingrese la base del triángulo: '))
+    h1=float(input('Ingrese la altura del triángulo: '))
     funciones.area_triangulo(base1,h1)
    
     
 if x==3:
     
-    base2=int(input('Ingrese la base del rectángulo: '))
-    h2=int(input('Ingrese la altura del rectángulo: '))
+    base2=float(input('Ingrese la base del rectángulo: '))
+    h2=float(input('Ingrese la altura del rectángulo: '))
     funciones.area_rectangulo(base2,h2)
     
