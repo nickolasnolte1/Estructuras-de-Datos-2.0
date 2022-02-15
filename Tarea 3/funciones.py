0252e20823f053181b0e7211d6271ec862899016
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-02-15 00:09:19 -0600

    .

---
+++ Tarea 3/funciones.py
@@ -0,0 +1,18 @@
+import math
+
+
+def area_circulo(radio):
+    
+    area1=math.pi*radio**2
+    return print('el area del circulo es: ', area1)
+
+def area_triangulo(b,h):
+
+    area2=b*h/2
+    return print('El area del triángulo es: ', area2)
+
+
+def area_rectangulo(b,h):
+
+    area3=b*h
+    return print('El area del Rectángulo es: ', area3)
---
+++ Tarea 3/profiling.py
@@ -0,0 +1,5 @@
+import cProfile
+import re 
+from runner import *
+
+cProfile.run('re.compile("areas")')
