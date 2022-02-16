27a485173ae643bfea602f0a566628be272570e0
Nickolas Nolte <nickolasnolte@ufm.edu>
2022-02-16 10:06:13 -0600

    api

--- Tarea 4/app.py
+++ Tarea 4/app.py
@@ -2,8 +2,6 @@ from crypt import methods
 from flask import Flask, render_template, request, url_for, redirect
 from jinja2 import Template, FileSystemLoader, Environment
 
-templates = FileSystemLoader('templates')
-environment = Environment(loader = templates)
 
 app = Flask(__name__)
 
@@ -11,7 +9,5 @@ app = Flask(__name__)
 def home():
     return render_template('Homepage.html')
 
-
-
 if __name__ == "__main__":
-    app.run(host="localhost", port = 5000, debug=True)
+    app.run(host="localhost", port = 8000, debug=True)
